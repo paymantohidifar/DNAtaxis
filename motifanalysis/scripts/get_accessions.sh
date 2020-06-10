fdfde4ad3f91e2770c53654b7cf7e84e69eae270
Payman Tohidifar <payman.che@gmail.com>
2020-06-10 14:56:51 -0500

    updated

--- motifanalysis/scripts/get_accessions.sh
+++ motifanalysis/scripts/get_accessions.sh
@@ -2,14 +2,15 @@
 
 # set up variables for directories
 rootdir=/home/payman/Research/dnataxis/motifanalysis
+metadir=$rootdir/meta
 refdir=$rootdir/reference_data
 
 # get number of species
-count=$(cat "$refdir/speciesNames.txt" | wc -l)
+count=$(cat "$metadir/speciesNames.txt" | wc -l)
 
 # retrieve description of Refseq for each species and write them into a file
 for ((i=1; i<=count; i++)); do
-    name=$(sed -n "$i"p "$refdir/speciesNames.txt")
+    name=$(sed -n "$i"p "$metadir/speciesNames.txt")
     datasets assembly_descriptors tax_name "$name" -r | \
     jq-linux64 '.datasets[] | {scientific_name: .org.sci_name, strain: .org.strain,
     tax_id: .org.tax_id, Assembly_level: .assembly_level, Assembly_accession: .assembly_accession,
